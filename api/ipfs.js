931668f7a4081cc0ad58b122f33842470a2407c2
Dutch4Doctor <ploegenmetpaarden@gmail.com>
2026-05-08 15:51:02 +0200

    Create ipfs.js

---
+++ api/ipfs.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  const { cid } = req.query;
+  if (!cid) return res.status(400).send('Missing cid');
+  const gateways = [
+    `https://api.universalprofile.cloud/ipfs/${cid}`,
+    `https://cloudflare-ipfs.com/ipfs/${cid}`,
+    `https://ipfs.io/ipfs/${cid}`,
+  ];
+  for (const url of gateways) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) continue;
+      const buffer = await response.arrayBuffer();
+      const contentType = response.headers.get('content-type') || 'image/jpeg';
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Content-Type', contentType);
+      res.setHeader('Cache-Control', 's-maxage=86400');
+      return res.send(Buffer.from(buffer));
+    } catch (e) { continue; }
+  }
+  res.status(404).send('Not found');
+}
