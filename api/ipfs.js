16f02b3b51003537a9cdff1499c1da55440c62e6
Dutch4Doctor <ploegenmetpaarden@gmail.com>
2026-05-08 16:17:10 +0200

    Update ipfs.js

--- api/ipfs.js
+++ api/ipfs.js
@@ -1,11 +1,14 @@
 export default async function handler(req, res) {
   const { cid } = req.query;
   if (!cid) return res.status(400).send('Missing cid');
+
   const gateways = [
     `https://api.universalprofile.cloud/ipfs/${cid}`,
     `https://cloudflare-ipfs.com/ipfs/${cid}`,
     `https://ipfs.io/ipfs/${cid}`,
+    `https://gateway.pinata.cloud/ipfs/${cid}`,
   ];
+
   for (const url of gateways) {
     try {
       const response = await fetch(url);
@@ -16,7 +19,10 @@ export default async function handler(req, res) {
       res.setHeader('Content-Type', contentType);
       res.setHeader('Cache-Control', 's-maxage=86400');
       return res.send(Buffer.from(buffer));
-    } catch (e) { continue; }
+    } catch (e) {
+      continue;
+    }
   }
-  res.status(404).send('Not found');
+
+  res.status(404).send('Image not found');
 }
